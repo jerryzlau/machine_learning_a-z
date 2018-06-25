cf4563e192f18958200a5241d9f73b1e5d2b9665
Jerry Lau <jerryzlau@gmail.com>
2018-06-24 20:46:52 -0700

    finished part 1

--- Part 1 - Data Preprocessing/my_work/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/my_work/data_preprocessing_template.py
@@ -16,7 +16,9 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
+"""
 # take care of missing data
+# fill in missing cells with the mean of that column
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
 imputer = imputer.fit(X[:, 1:3])
@@ -37,3 +39,19 @@ X = onehotencoder.fit_transform(X).toarray()
 # encode the purchased variable
 labelencoder_Y = LabelEncoder()
 y = labelencoder_Y.fit_transform(y)
+"""
+
+# split into training set and test set
+# training set trains the model
+# test set validates the accuracy of the model
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+"""
+# Feature scaling only need to be done to features that have a huge range
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+"""
+
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# machine_learning_a-z
+# Notes
+
+## Over-fitting
+Overfitting refers to a model that models the training data too well.
+
+
+## Feature Scaling
+Standardize the range of independent variables. It is splitted into standardization and normalization.
+
+### Standardization
+![Standardization](https://lh3.googleusercontent.com/t6fOG0XgBYkE5eOW0-QEgzazsJ2RBfYsuxejl1K_kDxk7Ip6_Pl1UVeWFOlHkBi6u6OTvTEQlNSO4ySsbp6S=w1915-h982)
+
+### Normalization
+![Normalization](https://lh5.googleusercontent.com/VmeK7iLUeSz2SWCSDcY26Sta0h428C0g_IyUcA7BK1GN4dLewpEmgAveyjw7O4e-fE7ICIlntuEWJvFS5lI3=w1915-h982)
